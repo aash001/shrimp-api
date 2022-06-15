8bf16ea5a9f556fb28c007663039e7e9868197ce
aash001 <ampinarioash@users.noreply.github.com>
2022-06-15 02:44:59 -0400

    Update db

--- src/main/resources/shrimps.sql
+++ src/main/resources/shrimps.sql
@@ -3,14 +3,14 @@ TRUNCATE shrimps;
 INSERT into 
 shrimps(id, name, image)
 VALUES
-(1, 'Red_Cherry','https://ik.imagekit.io/oiinm7zlx/shrimp/red-cherry-shrimp_YkDDNb4C3.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064771462'),
-(2,'Yellow_Rice','https://ik.imagekit.io/oiinm7zlx/shrimp/yellow-rice-shrimp_mK3ENxTfm.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064772152'),
-(3,'Sunkist_Orange','https://ik.imagekit.io/oiinm7zlx/shrimp/sunkist-orange-shrimp_vTw0f1EEJ6.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064771085'),
-(4,'Green_Jade','https://ik.imagekit.io/oiinm7zlx/shrimp/green-jade-shrimp_1DzDc7A8Bj.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064770617'),
-(5,'Blue_Dream','https://ik.imagekit.io/oiinm7zlx/shrimp/blue-dream-shrimp_tJbIELezY.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064770818'),
-(6,'Chocolate','https://ik.imagekit.io/oiinm7zlx/shrimp/chocolate-shrimp_p0CyPzAxi.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064772620'),
-(7,'Black_Onyx','https://ik.imagekit.io/oiinm7zlx/shrimp/black_shrimp_uiulvtaww8.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064770812'),
-(8,'Red_Rili','https://ik.imagekit.io/oiinm7zlx/shrimp/red-rili-shrimp_PaQn29eNjR.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064771311'),
-(9,'Orange_Rili','https://ik.imagekit.io/oiinm7zlx/shrimp/orange-rili-shrimp_7VZOhZtP1.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064772002'),
-(10,'Blue_Rili','https://ik.imagekit.io/oiinm7zlx/shrimp/blue-rili-shrimp_umz6jjBVz.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064771471'),
-(11,'Carbon_Rili','https://ik.imagekit.io/oiinm7zlx/shrimp/carbon-rili-shrimp_scQOAEQrn.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064770818');
+(3001, 'Red_Cherry','https://ik.imagekit.io/oiinm7zlx/shrimp/red-cherry-shrimp_YkDDNb4C3.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064771462'),
+(3002,'Yellow_Rice','https://ik.imagekit.io/oiinm7zlx/shrimp/yellow-rice-shrimp_mK3ENxTfm.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064772152'),
+(3003,'Sunkist_Orange','https://ik.imagekit.io/oiinm7zlx/shrimp/sunkist-orange-shrimp_vTw0f1EEJ6.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064771085'),
+(3004,'Green_Jade','https://ik.imagekit.io/oiinm7zlx/shrimp/green-jade-shrimp_1DzDc7A8Bj.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064770617'),
+(3005,'Blue_Dream','https://ik.imagekit.io/oiinm7zlx/shrimp/blue-dream-shrimp_tJbIELezY.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064770818'),
+(3006,'Chocolate','https://ik.imagekit.io/oiinm7zlx/shrimp/chocolate-shrimp_p0CyPzAxi.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064772620'),
+(3007,'Black_Onyx','https://ik.imagekit.io/oiinm7zlx/shrimp/black_shrimp_uiulvtaww8.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064770812'),
+(3008,'Red_Rili','https://ik.imagekit.io/oiinm7zlx/shrimp/red-rili-shrimp_PaQn29eNjR.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064771311'),
+(3009,'Orange_Rili','https://ik.imagekit.io/oiinm7zlx/shrimp/orange-rili-shrimp_7VZOhZtP1.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064772002'),
+(3010,'Blue_Rili','https://ik.imagekit.io/oiinm7zlx/shrimp/blue-rili-shrimp_umz6jjBVz.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064771471'),
+(3011,'Carbon_Rili','https://ik.imagekit.io/oiinm7zlx/shrimp/carbon-rili-shrimp_scQOAEQrn.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655064770818');
